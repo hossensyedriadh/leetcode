5e80398e6487dd93161a661e645ddae5adee7916
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2023-05-27 00:18:58 +0600

    Problem-1

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String... args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 15)));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        int[] indices = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i += 1) {
+            for (int j = i + 1; j < nums.length; j += 1) {
+                if (nums[i] + nums[j] == target) {
+                    indices[0] = i;
+                    indices[1] = j;
+                }
+            }
+        }
+
+        return indices;
+    }
+}
