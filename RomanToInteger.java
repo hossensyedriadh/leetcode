0b09ccd7e273827df7d407b1c9c254fad634257c
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2023-05-30 23:31:47 +0600

    Problem-13 improved

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -6,31 +6,25 @@ public class RomanToInteger {
         System.out.println(romanToInt("VMM"));
     }
 
-    //TODO: will try to do it in a better way later
     private static int romanToInt(String s) {
         final HashMap<Character, Integer> romanMap = new HashMap<>(Map.of('M', 1000, 'D', 500, 'C', 100,
                 'L', 50, 'X', 10, 'V', 5, 'I', 1));
 
-        char[] chars = s.toUpperCase().toCharArray();
         int converted = 0;
 
-        for (int i = 0; i < chars.length; i += 1) {
-            int n1 = romanMap.get(chars[i]);
-            if (i < chars.length - 1) {
-                int n2 = romanMap.get(chars[i + 1]);
-                if (n1 > n2) {
-                    converted += n1;
-                } else if (n2 > n1) {
-                    converted += (n2 - n1);
-                    i += 1;
-                } else {
-                    converted += n1;
-                }
+        for (int i = 0; i < s.length() - 1; i += 1) {
+            int current = romanMap.get(s.charAt(i));
+            int next = romanMap.get(s.charAt(i + 1));
+
+            if (current >= next) {
+                converted += current;
             } else {
-                converted += n1;
+                converted -= current;
             }
         }
 
-        return converted;
+        converted += romanMap.get(s.charAt(s.length() - 1));
+
+        return Math.abs(converted);
     }
 }
